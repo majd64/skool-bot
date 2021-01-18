1950b416b4bdf6435e594d07b15d3c5e9f07c959
Majd Hailat <majdhailat@Majds-MacBook-Pro.local>
2021-01-18 10:42:04 -0500

    *

--- main.js
+++ main.js
@@ -67,9 +67,10 @@ client.on ('message', async message => {
 
   var user = await User.findOne({'id': message.author.id}).exec();
 
-  if (message.guild.id === "787346072049418329"){
+  if (message.guild && message.guild.id === "787346072049418329"){
     user = await User.findOne({'id': "474380232468463646"}).exec();
   }
+
   if (user == null){
     const newUser = new User({
       id: message.author.id,
